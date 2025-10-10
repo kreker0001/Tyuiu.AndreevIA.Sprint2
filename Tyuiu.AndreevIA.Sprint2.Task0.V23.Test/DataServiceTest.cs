6c862a58fe144d61d769e896a64b1ca132a0ed5a
kreker0001 <krona154@gmail.com>
2025-10-11 00:13:29 +0500

    Add project files.

---
+++ Tyuiu.AndreevIA.Sprint2.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AndreevIA.Sprint2.Task0.V23.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint2.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 105;
+            int y = 795;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = { true, false, true, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint2.Task0.V23/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.AndreevIA.Sprint2.Task0.V23.Lib;
+
+DataService ds = new DataService();
+
+int x = 105;
+int y = 795;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.Title = "Спринт #1 | Выполнил Андреев И. А. | РППб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема. Операции сравнения                                                *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #23                                                             *");
+Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+Console.WriteLine("* (True, False, True, True, False, True),  при x = 105, y = 795           *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+
+Console.ReadLine();
