f0a6b06ebf9e0968ea82f80b35c55e3101f5928b
kreker0001 <krona154@gmail.com>
2025-10-16 21:04:29 +0500

    Task5

---
+++ Tyuiu.AndreevIA.Sprint2.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.AndreevIA.Sprint2.Task5.V3.Lib
+{
+    public class DataService : ISprint2Task5V3
+    {
+        public string FindDayName(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    return "понедельник";
+                case 2:
+                    return "вторник";
+                case 3:
+                    return "среда";
+                case 4:
+                    return "четверг";
+                case 5:
+                    return "пятница";
+                case 6:
+                    return "суббота";
+                case 7:
+                    return "воскресенье";
+                default:
+                    return "неверный день недели";
+            }
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.AndreevIA.Sprint2.Task5.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнил Андреев И. А. | РППб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Тернарный оператор                                                *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #3                                                              *");
+Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Составить программу, которая в зависимости от порядка номера дня недели *");
+Console.WriteLine("* (1, 2, ..., 7) выводит на экран его название (понедельник, вторник,     *");
+Console.WriteLine("* ..., воскресенье).                                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int value;
+
+Console.WriteLine("Введите значение value (от 1 до 7)");
+
+value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.FindDayName(value));
+Console.ReadKey();
