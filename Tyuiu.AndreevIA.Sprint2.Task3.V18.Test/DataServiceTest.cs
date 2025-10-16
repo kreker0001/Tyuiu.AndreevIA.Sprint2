d92b5ca31d24a06b2cc23710c74301edce097ef3
krona <krona@EBLAN>
2025-10-16 17:56:57 +0500

    Task3

---
+++ Tyuiu.AndreevIA.Sprint2.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.AndreevIA.Sprint2.Task3.V18;
+
+namespace Tyuiu.AndreevIA.Sprint2.Task3.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void TrueExpression1()
+        {
+            int x = 10;
+            var res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TrueExpression2()
+        {
+            int x = 0;
+            var res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TrueExpression3()
+        {
+            int x = 1;
+            var res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TrueExpression4()
+        {
+            int x = -30;
+            var res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint2.Task3.V18/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.AndreevIA.Sprint2.Task3.V18;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #2 | Выполнил Андреев И. А. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+Console.WriteLine("* трех знаков после запятой                                               *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Введите значение переменной X:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+var res = ds.Calculate(x);
+Console.WriteLine("Значение функции " + res);
+Console.ReadKey();
